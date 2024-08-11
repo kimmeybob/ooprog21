fadf9ea288dc15bfc6bc161dda154e148d48f248
carlsan <carlsankim@bposeats.com>
2024-08-12 03:16:01 +0800

    Initial Changes

---
+++ SampleProject.java
@@ -0,0 +1,11 @@
+public class SampleProject {
+    
+    public static void main(String[] a) {
+        String i = "Hello";
+        String o = " ";
+        String t = "World!";
+
+        System.out.println(i+o+t);
+    }
+}
+
